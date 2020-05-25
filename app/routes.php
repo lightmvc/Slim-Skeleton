bcc878226240e04f311b493b7da165aa41c4aa79
Andrew Caya <andrewscaya@yahoo.ca>
2020-05-25 16:17:09 -0400

    2020-05-25: Adds a default page.

--- app/routes.php
+++ app/routes.php
@@ -15,7 +15,26 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
+        $response->getBody()->write("<!DOCTYPE html>\n"
+            . "<html class=\"wf-brandongrotesque-n7-active wf-brandongrotesque-n3-active wf-active\">\n"
+            . "<head>\n"
+            . "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/all.css\">\n"
+            . "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\"/>\n"
+            . "<link rel=\"shortcut icon\" href=\"images/favicon.png\"/>\n"
+            . "</head>\n"
+            . "<body>\n"
+            . "<header class=\"site-header\">\n"
+            . "<div class=\"site-title\">\n"
+            . "<a href=\"http://www.slimframework.com/\">Slim</a>\n"
+            . "</div>\n"
+            . "<p class=\"site-slogan\">\n"
+            . "Welcome to the Slim Framework Skeleton Application!\n"
+            . "</p>\n"
+            . "</header>\n"
+            . "</body>\n"
+            . "</html>\n"
+        );
+
         return $response;
     });
 
